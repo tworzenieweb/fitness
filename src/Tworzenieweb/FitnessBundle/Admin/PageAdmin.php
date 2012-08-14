ec6868beef05db222031118b6f550f59ea8c6269
tworzenieweb <tworzenieweb@gmail.com>
2012-08-14 17:51:49 +0200

    some fixes

--- src/Tworzenieweb/FitnessBundle/Admin/PageAdmin.php
+++ src/Tworzenieweb/FitnessBundle/Admin/PageAdmin.php
@@ -36,7 +36,7 @@ class PageAdmin extends Admin
         $formMapper
                 ->with('General')
                 ->add('title')
-                ->add('body', 'html', array(
+                ->add('body', null, array(
                     'attr' => array(
                         'class' => 'tinymce',
                         'data-theme' => 'advanced' // simple, advanced, bbcode
